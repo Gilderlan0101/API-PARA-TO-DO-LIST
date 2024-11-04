00ef4413b81ed671087d517da7d0cedddac0fbed
Gilderlan silva da cruz <lansilva007gmail.com>
2024-11-04 16:07:59 -0300

    Tentativa N:5

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from src.server.services import servidor
 from src.controllers.api import *
 
 
+app = servidor.app 
+
 if __name__ == '__main__':
-    servidor.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
